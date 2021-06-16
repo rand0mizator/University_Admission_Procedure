762bdf60f95c66babebce147d041be9af581b68f
Rand0mizator <iosuslov@gmail.com>
2021-06-16 10:30:59 +0700

    Stage 4 Basic admissioning program admit students to their preffered departments accordingly to gpa

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -18,38 +18,39 @@ departments = {"Biotech": [],
                }
 # creating temporary duplicate of applicants list to track admitted applicants
 temp = applicants[:]
+
 # FIRST ROUND of admission, trying to admit student to its FIRST priority department
 for applicant in applicants:
     name, surname, gpa, dep1, dep2, dep3 = applicant
-    for department in [dep1]:
-        if len(departments[department]) < maximum_students:
-            departments[department].append(applicant)
-            temp.remove(applicant)
-            break
+    department = dep1
+    if len(departments[department]) < maximum_students:
+        departments[department].append(applicant)
+        temp.remove(applicant)
 # coping temp list with removed admitted in FIRST ROUND applicants to initial list of applicants
 applicants = temp[:]
 # sorting new applicants list by gpa and alphabeticaly
 applicants = sorted(applicants, key=lambda x: (-float(x[2]), x[0], x[1]))
+
 # SECOND ROUND of admission, trying to admit student to its SECOND priority department
 for applicant in applicants:
     name, surname, gpa, dep1, dep2, dep3 = applicant
-    for department in [dep2]:
-        if len(departments[department]) < maximum_students:
-            departments[department].append(applicant)
-            temp.remove(applicant)
-            break
+    department = dep2
+    if len(departments[department]) < maximum_students:
+        departments[department].append(applicant)
+        temp.remove(applicant)
 # coping temp list with removed admitted in SECOND ROUND applicants to initial list of applicants
 applicants = temp[:]
 # sorting new applicants list by gpa and alphabeticaly
 applicants = sorted(applicants, key=lambda x: (-float(x[2]), x[0], x[1]))
-# SECOND ROUND of admission, trying to admit student to its THIRD priority department
+
+# THIRD ROUND of admission, trying to admit student to its THIRD priority department
 for applicant in applicants:
     name, surname, gpa, dep1, dep2, dep3 = applicant
-    for department in [dep3]:
-        if len(departments[department]) < maximum_students:
-            departments[department].append(applicant)
-            temp.remove(applicant)
-            break
+    department = dep3
+    if len(departments[department]) < maximum_students:
+        departments[department].append(applicant)
+        temp.remove(applicant)
+
 # iterating through departments and printing students
 for department, students in departments.items():
     print()
